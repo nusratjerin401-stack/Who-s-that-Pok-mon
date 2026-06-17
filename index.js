f84763ecc9919b8f45669a0db4423fdfee0a98a5
Nusrat Jerin <nusratjerin401@gmail.com>
2026-06-17 14:02:46 -0400

    updated project

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require ("express");
+const axios = require("axios");
+
 
 const app = express();
 
@@ -21,6 +23,29 @@ app.get("/new",(req, res) => {
     hints: 5,
    });
 }); 
+
+app.get("/hint/:game_id/:n", async (req, res)=>{
+    const {game_id, n} = req.params;
+   const pokemonID = Number(
+    Buffer.from(game_id, "base64").toString()
+   );  
+   axios
+   .get(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`)
+   .then ((response)=> {
+    if (Number(n)===1) {
+        res.json({
+            hint:1,
+            value: response.data.moves[0]?.move?.name,
+        });
+    }
+   })
+   .catch(() => {
+    res.status(500).json ({
+        error: "Failed to fetch Pokemon",
+    });
+   });
+})
+
  app.listen(3000, ()=> {
     console.log("Server running on port 3000");
  });  
