3cc1feb5a36c74e944446ee515e4b349afca5ef1
Nusrat Jerin <nusratjerin401@gmail.com>
2026-06-17 13:18:45 -0400

    updated project

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ app .get("/", (req, res) => {
         message: "Who's That Pokémon API",
         });
     });
+
+
+app.get("/new",(req, res) => {
+    const randomPokemonID =
+    Math.floor(Math.random() * 1025) + 1;
+   const gameID = Buffer.from (
+    String(randomPokemonID)
+   ).toString("base64");
+
+   res.json({
+    game_id: gameID,
+    hints: 5,
+   });
+}); 
  app.listen(3000, ()=> {
     console.log("Server running on port 3000");
  });  
