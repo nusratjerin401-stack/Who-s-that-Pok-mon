5ad9088aaea728233268ca1543c395cd4ffc32f1
Nusrat Jerin <nusratjerin401@gmail.com>
2026-06-18 09:59:13 -0400

    updated project

--- index.js
+++ index.js
@@ -1,54 +1,58 @@
-const express = require ("express");
-const axios = require("axios");
-
-
+import express from "express";
 const app = express();
+app.use(express.json());
 
-app .get("/", (req, res) => {
-    res.json({
-        message: "Who's That Pokémon API",
-        });
-    });
+app.get("/", (req, res) => {
+  res.json({
+    message: "Who's That Pokémon API",
+  });
+});
 
-
-app.get("/new",(req, res) => {
-    const randomPokemonID =
+app.get("/new", (req, res) => {
+  const randomPokemonID =
     Math.floor(Math.random() * 1025) + 1;
-   const gameID = Buffer.from (
-    String(randomPokemonID)
-   ).toString("base64");
 
-   res.json({
+  const gameID = Buffer.from(
+    String(randomPokemonID)
+  ).toString("base64");
+
+  res.json({
     game_id: gameID,
     hints: 5,
-   });
-}); 
+  });
+});
 
-app.get("/hint/:game_id/:n", async (req, res)=>{
-    const {game_id, n} = req.params;
-   const pokemonID = Number(
+
+app.get("/hint/:game_id/:n", (req, res) => {
+  const { game_id, n } = req.params;
+
+  const pokemonID = Number(
     Buffer.from(game_id, "base64").toString()
-   );  
-   axios
-   .get(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`)
-   .then ((response)=> {
-    if (Number(n)===1) {
-        res.json({
-            hint:1,
-            value: response.data.moves[0]?.move?.name,
-        });
-    }
-   })
-   .catch(() => {
-    res.status(500).json ({
-        error: "Failed to fetch Pokemon",
-    });
-   });
-})
+  );
 
- app.listen(3000, ()=> {
-    console.log("Server running on port 3000");
- });  
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (Number(n) === 1) {
+        return res.json({
+          hint: 1,
+          value: data.moves[0]?.move?.name,
+        });
+      }
+
+      return res.status(400).json({
+        error: "Hint does not exist",
+      });
+    })
+    .catch(() => {
+      res.status(500).json({
+        error: "Failed to fetch Pokemon",
+      });
+    });
+});
+ 
+ app.listen(3000);
+  
 
  
  
